aad79f4ac0dbaf75bc315c53cc1318ded6f535ee
Jithinc1993 <jithincotp@gmail.com>
2023-02-21 09:54:49 +0530

    Initial commit

---
+++ create vpc and ec2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Set the VPC and subnet CIDR blocks
+VPC_CIDR_BLOCK="10.0.0.0/16"
+SUBNET_CIDR_BLOCK="10.0.1.0/24"
+
+# Create the VPC
+VPC_ID=$(aws ec2 create-vpc --cidr-block $VPC_CIDR_BLOCK --query 'Vpc.VpcId' --output text)
+
+# Enable DNS hostname support for the VPC
+aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-hostnames '{"Value":true}'
+
+# Create an Internet Gateway and attach it to the VPC
+IGW_ID=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text)
+aws ec2 attach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPC_ID
+
+# Create a public subnet within the VPC
+SUBNET_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block $SUBNET_CIDR_BLOCK --query 'Subnet.SubnetId' --output text)
+
+# Create a Route Table for the public subnet and add a default route to the Internet Gateway
+ROUTE_TABLE_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID --query 'RouteTable.RouteTableId' --output text)
+aws ec2 associate-route-table --route-table-id $ROUTE_TABLE_ID --subnet-id $SUBNET_ID
+aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID
+
+# Create a Security Group to allow SSH and Jenkins access to the instance
+SG_ID=$(aws ec2 create-security-group --group-name SSHAndJenkinsAccess --description "Security group for SSH and Jenkins access" --vpc-id $VPC_ID --query 'GroupId' --output text)
+aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 8080 --cidr 0.0.0.0/0
+
+# Launch an EC2 instance in the public subnet with the specified Security Group, key pair, and userdata
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type t2.micro --key-name my-key-pair --security-group-ids $SG_ID --subnet-id $SUBNET_ID --associate-public-ip-address --user-data "#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins") --query 'Instances[0].InstanceId' --output text
+
+# Wait for the instance to be in a running state
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+# Get the public IP address of the instance
+PUBLIC_IP=$(aws ec2 describe
