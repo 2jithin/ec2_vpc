76a12df7213f9a90e576bb126aa6bf7a1412f8c9
Jithinc1993 <jithincotp@gmail.com>
2023-03-09 13:45:00 +0530

    Updated

--- DockerInstallAmazonlinux.sh
+++ DockerInstallAmazonlinux.sh
@@ -42,7 +42,8 @@ sudo service docker stop
 sudo chmod 666 /var/run/docker.sock
 sudo systemctl daemon-reload
 sudo service docker restart
-
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.0-rc.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 source ~/.bash_profile") # --query 'Instances[0].InstanceId' --output text
 
 # added sonar group without user access bin/false
