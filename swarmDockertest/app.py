8aed3996c91b1b562c2d514506ada44e825b88c8
Jithinc1993 <jithincotp@gmail.com>
2023-03-19 13:55:01 +0530

    Fixed dockercompose issue

--- swarmDockertest/app.py
+++ swarmDockertest/app.py
@@ -4,7 +4,7 @@ import Redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+redis = Redis(host='redis', port=6379)
 
 def get_hit_count():
     retries = 5
@@ -17,8 +17,10 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-
 @app.route('/')
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
