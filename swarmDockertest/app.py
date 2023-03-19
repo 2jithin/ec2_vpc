bfa99a33529b202e9151d549f6806d94e777520e
Jithinc1993 <jithincotp@gmail.com>
2023-03-19 13:12:08 +0530

    modified python.py

--- swarmDockertest/app.py
+++ swarmDockertest/app.py
@@ -1,24 +1,22 @@
 import time
 
-from redis import Redis
+import Redis
 from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-# def get_hit_count():
-#     retries = 5
-#     while True:
-#         try:
-#             return cache.incr('hits')
-#         except redis.exceptions.ConnectionError as exc:
-#             if retries == 0:
-#                 raise exc
-#             retries -= 1
-#             time.sleep(0.5)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
 
 @app.route('/')
 def hello():
