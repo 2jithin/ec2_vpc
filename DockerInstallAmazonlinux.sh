1ec5ce1d5065a05e7501b2a838dcd1af8a3f0684
Jithinc1993 <jithincotp@gmail.com>
2023-03-31 13:41:27 +0530

    Updated

--- DockerInstallAmazonlinux.sh
+++ DockerInstallAmazonlinux.sh
@@ -87,6 +87,7 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl daemon-reload
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
+sudo chown ec2-user /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.0-rc.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 source ~/.bash_profile
